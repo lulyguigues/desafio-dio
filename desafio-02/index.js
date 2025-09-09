f76c188e7a996d738ee901f3ac663444a24969af
lulyguigues <lucianaguigues@gmail.com>
2025-09-09 16:48:23 -0300

    Refatora desafio 02 - separa funções calculaRank e calculaNivel

--- desafio-02/index.js
+++ desafio-02/index.js
@@ -1,5 +1,4 @@
-function calculaRank(vitorias, derrotas) {
-    let rank = (vitorias - derrotas);
+function calculaNivel(vitorias) {
     let nivel;
 
 if (vitorias < 10) {
@@ -16,24 +15,15 @@ if (vitorias < 10) {
   nivel = "Lendário";
 } else if (vitorias >= 101) {
     nivel = "Imortal"
+} return nivel
 }
+
+function calculaRank (vitorias, derrotas) {
+
+     let rank = (vitorias - derrotas);
+     let nivel = calculaNivel(vitorias);
+     
 console.log(`O Herói tem saldo de ${rank} e está no nível ${nivel}.`)
-
-/* Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
-
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**" */
 }
 
 function main () {
